0ddd9ae657178d838cafe5a6f3c2e8687d11eb12
Stephanie Julia Soares Dias <ssoaresdias9802@gmail.com>
2026-05-06 10:48:47 +0000

    classes

---
+++ Palavra.java
@@ -0,0 +1,9 @@
+public class Palavra{
+    String palavra;
+    int ocorrencias;
+
+    public Palavra(String palavra, int ocorrencias){
+        this.palavra = palavra;
+        this.ocorrencias  =ocorrencias;
+    }
+}
