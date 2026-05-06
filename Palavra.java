fb7cafaea1489f31a6479e486dea528a404aeaee
Stephanie Julia Soares Dias <ssoaresdias9802@gmail.com>
2026-05-06 11:10:13 +0000

    pk

--- Palavra.java
+++ Palavra.java
@@ -6,4 +6,30 @@ public class Palavra{
         this.palavra = palavra;
         this.ocorrencias  =ocorrencias;
     }
+
+    public void lerArquivo(){
+        try {
+            FileReader arq = new FileReader("Discurso1.txt");
+            BufferedReader linha = new BufferedReader(arq);
+            FileReader arq2 = new FileReader("Discurso2.txt"); 
+
+
+            int pos = 0; 
+
+            while(aux != null) {
+                // separa dentro de vetor sempre que encontra virgula
+                String[] dados = aux.split(",");
+                ABB d1 = new Exame(dados[0],dados[1],Integer.parseInt(dados[2]));
+                listaExamesDisponiveis.add(pos,ex);
+                pos++;
+                // proximo exame
+                aux = linha.readLine();
+            }
+            arq.close();
+        }
+        catch(Exception e) {
+            System.out.println("Erro na leitura do arquivo: " + e.getMessage());
+        }
+        
+    }
 }
